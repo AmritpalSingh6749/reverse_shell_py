66a73ebd3dd264ea2cc3837048fcadae4184fde2
AmritpalSingh6749 <63670270+AmritpalSingh6749@users.noreply.github.com>
2020-10-13 11:09:14 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -18,31 +18,31 @@ def connectSocket():
   global port
   global host
   # Combining socket, port, and ip to listen to connection
-  print"# Binding to port }:-) "+str(port)
-  sock_s.bind(host, port)
+  print ("# Binding to port }:-) "+str(port))
+  sock_s.bind((host, port))
   # How many connections u want to accept, as default i am setting it to 1
   sock_s.listen(1)
   # Accept functions returns socket and address of incomming conection
   sock_c, addr=sock_s.accept()
-  print "#Connection established with }:-) "+addr[0]+":"+str(addr[1])
+  print ("\n #Connection established with }:-) "+addr[0]+":"+str(addr[1]))
   talkClient(sock_c)
   # It is wise to close your connection.... you are not the only evil }:-)
   sock_c.close()
   sock_s.close()
 
 def talkClient(conn):
-  print"# Enter your commands after each reply u get }:-) \n"
+  print ("\n# Enter your commands after each reply u get }:-) \n")
   # Keep alive connection till u want to exit
   while true:
-    msg=input()
-    if msg=="quit":
+    msg=input(">")
+    if (msg=="quit"):
       return
     # To check if somehing is being sent or not
-    if len(msg)>0:
-      conn.send(msg)
-    # How many bytes u want to receive
-    rply=str(conn.recv(1024))
-    print (rply, end="")
+    if (len(msg)>0):
+      conn.send(str.encode(msg)
+    # How many bytes u want to receive... here it is 1024
+    rply=str.decode(conn.recv(1024),"utf 8")
+    print ("\n"+rply, end="")
    
 
 def main():
