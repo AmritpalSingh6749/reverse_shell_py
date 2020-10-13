426cf9b1d1638f130306e2d85ce30ba5e789abc2
AmritpalSingh6749 <63670270+AmritpalSingh6749@users.noreply.github.com>
2020-10-13 12:49:09 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -2,38 +2,30 @@
 # It is expected that you have basic knowledge about ip and port
 import socket     # to create the socket to listen to incoming connection 
 import sys        # To execute command on the host system
-
-# Defining function in python to create a socket 
-def socketCreate(): 
-  global host=""
-  global port=8080
-  if int(sys.argv[0]):
-    port=int(sys.argv[0])
-  # AF_INET is for address family ipv4, to use ipv6 change it with AF_INET6
-  # SOCK_STREAM is to create TCP socket and SOCK_DGRAM is to create UDP socket
-  global sock_s=socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+host=""
+port=8080
+# AF_INET is for address family ipv4, to use ipv6 change it with AF_INET6
+# SOCK_STREAM is to create TCP socket and SOCK_DGRAM is to create UDP socket
+sock_s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 def connectSocket():
-  global sock_s
-  global port
-  global host
-  # Combining socket, port, and ip to listen to connection
-  print ("# Binding to port }:-) "+str(port))
-  sock_s.bind((host, port))
-  # How many connections u want to accept, as default i am setting it to 1
-  sock_s.listen(1)
-  # Accept functions returns socket and address of incomming conection
-  sock_c, addr=sock_s.accept()
-  print ("\n #Connection established with }:-) "+addr[0]+":"+str(addr[1]))
-  talkClient(sock_c)
-  # It is wise to close your connection.... you are not the only evil }:-)
-  sock_c.close()
-  sock_s.close()
+    # Combining socket, port, and ip to listen to connection
+    print ("# Binding to port }:-) "+str(port))
+    sock_s.bind((host, port))
+    # How many connections u want to accept, as default i am setting it to 1
+    sock_s.listen(1)
+    # Accept functions returns socket and address of incomming conection
+    sock_c, addr=sock_s.accept()
+    print ("\n #Connection established with }:-) "+addr[0]+":"+str(addr[1]))
+    talkClient(sock_c)
+    # It is wise to close your connection.... you are not the only evil }:-)
+    sock_c.close()
+    sock_s.close()
 
 def talkClient(conn):
   print ("\n# Enter your commands after each reply u get }:-) \n")
   # Keep alive connection till u want to exit
-  while true:
+  while True:
     msg=input(">")
     if (msg=="quit"):
       return
@@ -46,6 +38,6 @@ def talkClient(conn):
    
 
 def main():
-  socketCreate()
   connectSocket()
+
 main() 
